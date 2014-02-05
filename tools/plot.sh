642448f63effd193bd959e1c57c785507e287982
Serge Ribalchenko <fisher@tpaba.org.ua>
2014-02-05 13:49:22 +0200

    added some visualization helper tools

---
+++ tools/plot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gnuplot -e 'set xrange [0:10]; set yrange [0:10]; plot "wikipedia.dat"; pause -1'
+
---
+++ tools/plot3d.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env gnuplot
+
+set xrange [-12:12];
+set yrange [-12:12];
+set zrange [-12:12];
+splot "sdf.dat";
+pause -1;
+
